764abae0a8ca5d4a6aae47fa41d7e36ceef0914b
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2024-03-15 14:40:01 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme/theme";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./router/Router";
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ChakraProvider>
+  );
+}
+
+export default App;
---
+++ src/router/Router.tsx
@@ -0,0 +1,31 @@
+import { Route, Routes } from "react-router-dom";
+import { Login } from "../components/pages/Login";
+import { HomeRoutes } from "./HomeRoutes";
+import { Page404 } from "../components/pages/Page404";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
+import { LoginUserProvider } from "../providers/LoginUserProvider";
+
+export const Router = () => {
+  return (
+    <LoginUserProvider>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/home/*"
+          element={
+            <Routes>
+              {HomeRoutes.map((route) => (
+                <Route
+                  key={route.path}
+                  path={route.path}
+                  element={<HeaderLayout>{route.children}</HeaderLayout>}
+                />
+              ))}
+            </Routes>
+          }
+        />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </LoginUserProvider>
+  );
+};
